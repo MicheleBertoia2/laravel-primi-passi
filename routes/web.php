f74469cc9f4ec3c1c41413f9de59ec68d25af865
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-06-01 15:49:49 +0200

    [add] basic structure with route

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    $name = 'Michele';
+    $surname = 'Bertoia';
+    return view('home', compact('name','surname'));
+})->name('home');
